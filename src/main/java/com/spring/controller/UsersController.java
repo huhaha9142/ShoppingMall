813fcafa1d237df0a036d72ee74dd29473ce2d69
pvpvpvpvp <77872841+pvpvpvpvp@users.noreply.github.com>
2021-12-14 16:27:02 +0900

    메일 링크 수정

--- src/main/java/com/spring/controller/UsersController.java
+++ src/main/java/com/spring/controller/UsersController.java
@@ -213,7 +213,7 @@ public class UsersController {
 			MimeMessage mail = mailSender.createMimeMessage();
 			String htmlStr = "<h2>안녕하세요 MS :p CUSTOM SHOPPINGMALL 입니다!</h2><br><br>" 
 					+ "<h3>" + id + "님</h3>" + "<p>인증하기 버튼을 누르시면 로그인을 하실 수 있습니다 : " 
-					+ "<a href='http://ec2-3-37-117-153.ap-northeast-2.compute.amazonaws.com:8080/shoppingmall"+"/user-join-email?id="+ id +"&key="+key+"'>인증하기</a></p>"
+					+ "<a href='http://localhost"+"/email-cerified?id="+ id +"&key="+key+"'>인증하기</a></p>"
 					+ "(혹시 잘못 전달된 메일이라면 이 이메일을 무시하셔도 됩니다)";
 			try {
 				mail.setSubject("[본인인증] MS :p CUSTOM SHOPPINGMALL의 인증메일입니다", "utf-8");
