a0eb8b7bcc17c9138e4f84724925df2768a27ff7
pvpvpvpvp <77872841+pvpvpvpvp@users.noreply.github.com>
2021-12-28 17:42:16 +0900

    제품 추가 살리기

--- src/main/java/com/spring/function/FunctionSpring.java
+++ src/main/java/com/spring/function/FunctionSpring.java
@@ -211,24 +211,5 @@ public class FunctionSpring {
     		.parseClaimsJws(jwtToken)
     		.getBody();
     }
- // 이메일 난수 만드는 메서드
- 	public String init(boolean lowerCheck,int size) {
- 		Random ran = new Random();
- 		StringBuffer sb = new StringBuffer();
- 		int num = 0;
-
- 		do {
- 			num = ran.nextInt(75) + 48;
- 			if ((num >= 48 && num <= 57) || (num >= 65 && num <= 90) || (num >= 97 && num <= 122)) {
- 				sb.append((char) num);
- 			} else {
- 				continue;
- 			}
-
- 		} while (sb.length() < size);
- 		if (lowerCheck) {
- 			return sb.toString().toLowerCase();
- 		}
- 		return sb.toString();
- 	}
+ 
 }
